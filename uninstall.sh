225e7aa2f3fe254a45188ed2eb9a840589984685
kodumbeats <brandon.leckemby@gmail.com>
2020-09-11 17:31:21 -0400

    add verbosity to uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -5,7 +5,12 @@ rm -r src/
 rm -r min/
 rm -r pkg/
 
-echo "removing installed files"
+echo "removing the installed files: "
+echo "/usr/bin/min"
+echo "/usr/share/applications/min.desktop"
+echo "/usr/lib/min"
+echo "/usr/share/icons/hicolor/256x256/apps/min.png"
+echo "and I need root access for this"
 sudo rm /usr/bin/min
 sudo rm  /usr/share/applications/min.desktop
 sudo rm -r /usr/lib/min/
